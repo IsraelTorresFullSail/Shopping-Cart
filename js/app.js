4e759355b0cb48b57e61b3339aeffe20e771798a
Israel Torres <israel.a.torres01@gmail.com>
2019-10-09 00:44:29 -0400

    getting and displying product are working

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@ window.addEventListener("load", function() {
 class ShoppingCart {
     constructor() {
         console.log("Singleton Created");
-        var controller = new Controller();
+        
+        const ui = new UI();
+        const products = new Products();
+
+        // get all products
+        products.getProducts().then(products => ui.displayProducts(products));
     }
 
     static getInstance() {
@@ -24,28 +29,43 @@ class ShoppingCart {
     }
 }
 
-class Controller {
-    constructor(model, view) {
-        console.log("Controller Created");
-        this.model = new Model();
-        this.view = new View();
-    }
-}
-
-class Model {
-    constructor(id, name, description, price, image) {
-        console.log("Model Created");
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.image = image;
-    }
-}
-
-class View {
-    constructor() {
-        console.log("View Created");
+// Getting the products
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('/js/products.json');
+            let data = await result.json();
+                
+            let products = data.items;
+            products = products.map( item => {
+                const {company, name, description, price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {company, name, description, price, id, image};
+            })
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
         
+    }
+}
+
+// display products
+class UI {
+    displayProducts(products) {
+        let result = '';
+        products.forEach( product => {
+            result +=`
+                <div class="product-card">
+                    <p class="p-small">${product.company}</p>
+                    <h2>${product.name}</h2>
+                    <img src=${product.image} alt=${product.name}>
+                    <p class="price">${product.price}</p>
+                    <button class="add" data-id=${product.id}><i class="fas fa-plus"></i></button>
+                </div>
+            `
+        });
+        document.querySelector('.product-grid').innerHTML = result;
     }
 }
